bb18e48097f4c98ffbd53f472b4868aa30b2480d
OsvaldoRuiz622 <142273691+OsvaldoRuiz622@users.noreply.github.com>
2023-11-10 12:59:31 -0600

    Add files via upload

---
+++ Libreria_script.sql
@@ -0,0 +1,17 @@
+create database libreria
+-- Crear la tabla Autor
+CREATE TABLE Autor (
+    autor_id INT PRIMARY KEY,
+    nombre NVARCHAR(255)
+);
+
+-- Crear la tabla Libro con una relación de 1 a muchos con Autor
+CREATE TABLE libro (
+    id_libro INT PRIMARY KEY,
+    titulo NVARCHAR(255),
+    capitulos INT,
+    paginas INT,
+    precio INT,
+    id_autor INT,
+    FOREIGN KEY (id_autor) REFERENCES Autor(autor_id)
+);
